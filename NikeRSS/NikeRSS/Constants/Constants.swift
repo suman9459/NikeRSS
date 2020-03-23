7dd2e1888800bb9fa103118b9c8a67c0e206ea64
suman9459 <37310701+suman9459@users.noreply.github.com>
2020-03-23 08:55:27 -0500

    Updated URL

--- NikeRSS/NikeRSS/Constants/Constants.swift
+++ NikeRSS/NikeRSS/Constants/Constants.swift
@@ -10,5 +10,5 @@ import Foundation
 import UIKit
 
 class Constants: NSObject {
-    static let baseUrl = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/10/explicit.json"
+    static let baseUrl = "https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/100/explicit.json"
 }
